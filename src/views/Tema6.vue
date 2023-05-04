34c59905d6ac836e3b10f5b55c5040f7653ef5c9
elrilacho <camilobr17@gmail.com>
2023-05-04 11:28:52 -0500

    tema7_listo

--- src/views/Tema6.vue
+++ src/views/Tema6.vue
@@ -5,8 +5,69 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 6
-      h1 
-
+      h1 Modelo relacional 
+    p.mb-2(data-aos='fade-right') Para mejor ilustración de lo que es un modelo relacional y complementar lo que ya se ha visto hasta ahora sobre el mismo, es preciso comenzar con un ejemplo simple de dos tablas que una pequeña empresa podría utilizar para procesar pedidos de sus productos.
+    .f6.mn.p-5
+      .row.justify-content-center.mb-5
+        .col-lg-7.mb-lg-0.mb-3(data-aos="fade-right")
+          .tarjeta.p-4.mb-3(style="background-color: #D5CEF8 ")
+            p La primera tabla es una tabla de información del cliente, por lo que cada registro incluye el nombre, la dirección, la información de envío y facturación, el número de teléfono y otra información de contacto. Cada bit de información (cada atributo) está en su propia columna y la base de datos asigna un ID único (una clave) a cada fila. 
+              br
+              br
+              | En la segunda tabla, una tabla del pedido del cliente, cada registro incluye el ID del cliente que realizó el pedido, el producto solicitado, la cantidad, el tamaño y el color seleccionados, etcétera, pero no el nombre o la información de contacto del cliente.
+              br
+              br
+              | Estas dos tablas tienen solo una cosa en común: la columna de ID (la clave). 
+              br
+              br
+              | Gracias a esa columna en común, la base de datos relacional puede crear una relación entre las dos tablas. Entonces, cuando la aplicación de procesamiento de pedidos de la empresa envía un pedido a la base de datos, la base de datos puede ir a la tabla de pedidos del cliente, extraer la información correcta sobre el pedido del producto y usar el ID del cliente de esa tabla para buscar la información de facturación y envío en la tabla de información del cliente. 
+              br
+              br
+              | Entonces, el almacén puede extraer el producto correcto, el cliente puede recibir la entrega del pedido a tiempo y la empresa puede recibir el pago. 
+          p El modelo relacional significa que las estructuras lógicas de datos (las tablas de datos, las vistas y los índices) están separadas de las estructuras físicas de almacenamiento. 
+        .col-lg-5.col-6(data-aos="fade-left"): img(src='@/assets/curso/temas/tema6/1.png', alt='')
+      .tarjeta.p-4.mb-4(style="background-color: #A3B9CF")
+        .row.justify-content-center
+          .col-lg-1.col-6: img(src='@/assets/curso/temas/tema6/2.svg', alt='')
+          .col-lg-11
+            p.mb-0 Esta separación significa que los administradores de bases de datos pueden administrar el almacenamiento físico de datos sin afectar el acceso a esos datos como una estructura lógica. Por ejemplo, cambiar el nombre de un archivo de base de datos no cambia el nombre de las tablas almacenadas en él.
+      .row.justify-content-center.mb-5
+        .col-lg-4.col-6: img(src='@/assets/curso/temas/tema6/3.png', alt='')
+        .col-lg-8
+          .tarjeta.color-secundario.p-4.mb-4
+            .row.justify-content-center.align-items-center
+              .col-lg-2.col-4(data-aos="fade-right"): img(src='@/assets/curso/temas/tema1/9.svg', alt='')
+              .col-lg-10
+                p.text-white.mb-0 La distinción entre lógica y física también se aplica a las operaciones de la base de datos, que son acciones claramente definidas que permiten a las aplicaciones manipular los datos y las estructuras de la base de datos. Las operaciones lógicas permiten que una aplicación especifique el contenido que necesita, mientras que las operaciones físicas determinan cómo se debe acceder a esos datos y luego realizan la tarea.
+          p Para garantizar que los datos sean siempre precisos y accesibles, las bases de datos relacionales siguen ciertas reglas de integridad. Por ejemplo, una regla de integridad puede especificar que no se permitan filas duplicadas en una tabla, para eliminar la posibilidad de que se ingrese información errónea en la base de datos, #[span.color-primario como se ilustra en la imagen a continuación.]
+      .row.justify-content-center.mb-5
+        .col-lg-8
+          .titulo-sexto.color-acento-contenido(data-aos='fade-right')
+            h5 Figura 12
+            br
+            span.fst-italic Modelo relacional
+          img(src='@/assets/curso/temas/tema6/4.svg', alt='').mb-3
+          figcaption.bg-white Nota. Adaptado de Araneda (2020).
+      p.mb-5(data-aos='fade-right')
+        span.color-primario Observe a continuación, a través de un video, cómo se llega al modelo relacional:
+      figure
+        .video
+          iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+    .subtitulo.mb-3
+      h3 Modelo relacional y coherencia de datos
+    p.mb-5(data-aos='fade-right') El modelo relacional es el mejor para mantener la consistencia de los datos en todas las aplicaciones y copias de la base de datos (denominadas instancias). 
+    .row.justify-content-center.mb-5
+      .col-lg-7.mb-lg-0.mb-3
+        p Por ejemplo, cuando un cliente deposita dinero en un cajero automático y luego mira el saldo de la cuenta en un teléfono móvil, el cliente espera ver que ese depósito se refleje inmediatamente en un saldo de cuenta actualizado. Las bases de datos relacionales se destacan en este tipo de consistencia de datos, lo que garantiza que múltiples instancias de una base de datos tengan los mismos datos todo el tiempo.
+          br
+          br
+          |Es difícil para otros tipos de bases de datos mantener este nivel de coherencia oportuna con grandes cantidades de datos. Algunas bases de datos recientes, como NoSQL, solo pueden proporcionar “coherencia eventual”.
+        .tarjeta.color-secundario.p-4.mb-4
+          .row.justify-content-center.align-items-center
+            .col-lg-2.col-4(data-aos="fade-right"): img(src='@/assets/curso/temas/tema1/9.svg', alt='')
+            .col-lg-10
+              p.text-white.mb-0 Según este principio, cuando se escala la base de datos o cuando varios usuarios acceden a los mismos datos al mismo tiempo, los datos necesitan algo de tiempo para “ponerse al día”. La coherencia eventual es aceptable para algunos usos, como mantener listados en un catálogo de productos, pero para operaciones comerciales críticas, como transacciones de carrito de compras, la base de datos relacional sigue siendo lo ideal. 
+      .col-lg-5.col-6: img(src='@/assets/curso/temas/tema6/5.png', alt='')
 </template>
 
 <script>
@@ -26,4 +87,10 @@ export default {
 }
 </script>
 
-<style lang="sass"></style>
+<style lang="sass">
+.f6
+  background-image: url('~@/assets/curso/temas/tema6/f6.svg')
+  background-repeat: no-repeat
+  background-position: right bottom
+  background-size: cover
+</style>
--- src/views/Tema7.vue
+++ src/views/Tema7.vue
@@ -5,8 +5,185 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 7
-      h1 
+      h1 Diagrama entidad-relación
+    .row.justify-content-center.mb-5
+      .col-lg-1.col-5.mb-lg-0.mb-3(data-aos="fade-right"): img(src='@/assets/curso/temas/tema7/1.svg', alt='')
+      .col-lg-11(data-aos="fade-left")
+        p.mb-0 El Modelo Entidad Relación (MER) es una herramienta de modelado que fue introducida originalmente por Peter Chen, en 1976, y aunque ha sufrido variaciones en cuanto a los elementos de diagramas utilizados para representar sus elementos, su operación y utilidad siguen vigentes. 
+    .row.justify-content-center.mb-5
+      .col-lg-7.mb-lg-0.mb-3(data-aos="fade-right")
+        p La base del MER está en identificar los elementos o entidades importantes del sistema, los datos (atributos) que componen cada uno de ellos y la interacción (relación) entre dichos elementos.
+          br
+          br
+          |El MER consiste en representar, a nivel conceptual, los datos que soportan el funcionamiento de un sistema y sus componentes básicos son: Entidades, Atributos y Relaciones. 
+        .tarjeta.bg-tema2__1.px-3.py-4.mb-3
+            p.mb-0.ms-5 Las entidades representan abstracciones con atributos que almacenan datos; las relaciones son las asociaciones que existen entre entidades y permiten generar información al combinar diferentes entidades.
+        p Hasta este momento ya se ha dado una introducción a estos componentes básicos, pero para profundizar podrá ver un poco más sobre su conceptualización:
+      .col-lg-5.col-6(data-aos="fade-left"): img(src='@/assets/curso/temas/tema7/2.png', alt='')
+    AcordionA.mb-5(tipo="a" clase-tarjeta="tarjeta tarjeta--gris")
+      .row(titulo="Entidad")
+        .col-md-9.mb-4.mb-md-0
+          p Se denomina entidad a todo ente (conceptual o físico) del cual se desea establecer su participación dentro de un sistema de información. Una entidad concreta o física es aquella con existencia física, representa un objeto del mundo real (persona o elemento). Una entidad abstracta no tiene una representación física concreta (posición laboral, asignatura).
+        .col-md-3
+          figure
+            img(src='@/assets/curso/temas/tema7/3.svg', alt='Texto que describa la imagen')
+      .row(titulo="Atributo")
+        .col-md-9.mb-4.mb-md-0
+          p El atributo es un elemento de información que caracteriza a una entidad, identificándola, calificándola, cuantificándola, o declarando su estado. Por lo general, una entidad se compone de uno o más atributos (edad, genero, estatura, nombre, etc.). Los atributos permiten diferenciar elementos dentro de un conjunto de entidades. Dentro de una entidad de tipo persona, es muy raro el caso que existan dos con exactamente los mismos atributos.
+        .col-md-3
+          figure
+            img(src='@/assets/curso/temas/tema7/4.svg', alt='Texto que describa la imagen')
+      .row(titulo="Relaciones")
+        .col-md-9.mb-4.mb-md-0
+          p Las relaciones identifican la interacción que existe entre dos o más entidades. Establecen el comportamiento del sistema de información. 
+        .col-md-3
+          figure
+            img(src='@/assets/curso/temas/tema7/5.svg', alt='Texto que describa la imagen')
+    .f7.p-5.mn.pb-2 
+      p #[span.color-primario Los elementos básicos de MER] se presentan en un diagrama simple, que permite establecer en forma general un modelo de datos. Observe, entonces, cuáles son estos elementos fundamentales del MER:
+      .row.justify-content-center.mb-5
+        .col-lg-8
+          img(src='@/assets/curso/temas/tema7/6.png', alt='Texto que describa la imagen')
+      p Un ejemplo de uso sería el siguiente:
+      .row.justify-content-center.mb-5
+        .col-lg-8
+          .titulo-sexto.color-acento-contenido(data-aos='fade-right')
+            h5 Figura 12 
+            br
+            span.fst-italic  Ejemplo MER
+          img(src='@/assets/curso/temas/tema7/7.png', alt='Texto que describa la imagen')
+      .subtitulo.mb-3
+        h3 Modelamiento lógico
+      .cajon-b.color-primario.arriba-derecha.p-3.mb-5
+        .row.justify-content-center.align-items-center
+          .col-lg-10
+            p.mb-0 Un modelo de datos lógicos describe los datos con el mayor detalle posible, independientemente de cómo se implementarán físicamente en la base de datos.
+              br
+              br
+              |El mismo también cuenta con características propias y etapas para realizar, #[span.color-primario las cuales se muestran a continuación:]
+          .col-2(data-aos="fade-left"): img(src='@/assets/curso/temas/tema5/17.svg', alt='' style="width: 100px") 
+      .titulo-sexto.color-acento-contenido(data-aos='fade-right')
+        h5 Tabla 2
+        br
+        span.fst-italic Características y etapas del modelamiento lógico
+      .tabla-a.color-acento-contenido.mb-5
+        table
+          thead(style="background-color: #64B2FF ")
+            tr
+              th.h5.fw-bold Características
+              th.h5.fw-bold  Etapas
+          tbody.bg-white
+            tr
+              td 
+                ul.lista-ul--color
+                  li
+                    i.fas.fa-check
+                    | Incluye todas las entidades y relaciones entre ellos.
 
+                  li
+                    i.fas.fa-check
+                    | Todos los atributos para cada entidad están especificados.
+
+                  li.fw-bold
+                    i.fas.fa-check
+                    | La clave principal para cada entidad está especificada.
+
+                  li.fw-bold
+                    i.fas.fa-check
+                    | Se especifican las claves externas (claves que identifican la relación entre diferentes entidades).
+
+                  li.fw-bold
+                    i.fas.fa-check
+                    | La normalización ocurre en este nivel.
+              td.fw-bold 
+                ul.lista-ul--color
+                  li
+                    i.fas.fa-check
+                    | Especifique claves primarias para todas las entidades.
+
+                  li
+                    i.fas.fa-check
+                    | Encuentre las relaciones entre diferentes entidades.
+
+                  li
+                    i.fas.fa-check
+                    | Encuentre todos los atributos para cada entidad.
+
+                  li
+                    i.fas.fa-check
+                    | Resuelva las relaciones de muchos a muchos.
+
+                  li
+                    i.fas.fa-check
+                    | Normalización.
+    .subtitulo.mb-5
+      h3 Cardinalidad
+    .bloque-texto-g.color-secundario.p-3.p-sm-4.p-md-5.mb-4
+      .bloque-texto-g__img(
+        :style="{'background-image':`url(${require('@/assets/curso/temas/tema7/9.jpg')})`}"
+      )
+      .bloque-texto-g__texto.p-5
+        p.mb-0 La cardinalidad está definida como la cantidad de elementos, en términos de proporción, que participan en la relación entre dos o más entidades. Esta puede ser entre elementos únicos (unitarios) o múltiples. Generalmente, se utiliza la denominación “1” para elementos unitarios y “N” para varios elementos participantes.
+    p.mb-5(data-aos='fade-right') Por ejemplo, el diagrama siguiente expresa que “un autor escribe varios libros” o “un libro es escrito por un autor”:
+    .row.justify-content-center.mb-5
+      .col-lg-8
+        .titulo-sexto.color-acento-contenido(data-aos='fade-right')
+          h5 Figura 14
+          br
+          span.fst-italic Ejemplo Cardinalidad 
+        img(src='@/assets/curso/temas/tema7/10.png', alt='Texto que describa la imagen')
+    p.mb-5(data-aos='fade-right') Las expresiones que pueden indicarse a partir de estos 4 diagramas son:
+    .f7.mn.p-5
+      .row.justify-content-center.mb-5
+        .col-lg-3.mb-lg-0.mb-3
+          .tarjeta.bg-tema4__2.p-4.h-100
+            img(src="../assets/curso/temas/tema7/11.svg", alt="tema4-4" style="width: 77px").mb-4
+            p.mb-0 Un Autor escribe Al Menos un Libro (Cardinalidad mínima=1).
+        .col-lg-3.mb-lg-0.mb-3
+          .tarjeta.bg-tema4__2.p-4.h-100
+            img(src="../assets/curso/temas/tema7/12.svg", alt="tema4-4" style="width: 77px").mb-4
+            p.mb-0 Un Autor escribe Varios Libros (Cardinalidad máxima = N).
+        .col-lg-3.mb-lg-0.mb-3
+          .tarjeta.bg-tema4__2.p-4.h-100
+            img(src="../assets/curso/temas/tema7/13.svg", alt="tema4-4" style="width: 77px").mb-4
+            p.mb-0 Un Autor escribe uno o Varios Libros (Cardinalidad mínima= 1, máxima =N).
+        .col-lg-3.mb-lg-0.mb-3
+          .tarjeta.bg-tema4__2.p-4.h-100
+            img(src="../assets/curso/temas/tema7/14.svg", alt="tema4-4" style="width: 77px").mb-4
+            p.mb-0 Un Libro es escrito por un solo un Autor (Cardinalidad mínima y máxima = 1) .
+      p.mb-5(data-aos='fade-right') Otro ejemplo que se podría traer como práctica es el siguiente, donde se desea colocar la cardinalidad correcta, de acuerdo con el tipo de relación que se refleja dentro de la base de datos, y es posible observarlo en la figura siguiente:
+      .row.justify-content-center.mb-5
+        .col-lg-8
+          .titulo-sexto.color-acento-contenido(data-aos='fade-right')
+            h5 Figurqa 15 
+            br
+            span.fst-italic Ejemplo Cardinalidad
+          img(src='@/assets/curso/temas/tema7/15.png', alt='Texto que describa la imagen')
+      .row.justify-content-center.align-items-center.mb-5
+        .col-lg-1.col-5.mb-lg-0.mb-3: img(src='@/assets/curso/temas/tema7/16.svg', alt='Texto que describa la imagen')
+        .col-lg-11
+          p Una vez ya se han visto reflejados los datos dentro de un sistema de bases de datos real, en este caso, se está utilizando el motor de base de datos MySQL. 
+            br
+            br
+            | Es posible observar cómo quedan distribuidas las relaciones de acuerdo con las conexiones que se establecen en la base de datos; es decir, se pasa del modelo conceptual de entendimiento básico a ya crear un sistema de bases de datos con todas sus relaciones completas, como se puede visualizar a continuación:
+      .row.justify-content-center.mb-5
+        .col-lg-8
+          .titulo-sexto.color-acento-contenido(data-aos='fade-right')
+            h5 Figurqa 16 
+            br
+            span.fst-italic Ejemplo Sistema de bases de datos
+          img(src='@/assets/curso/temas/tema7/17.png', alt='Texto que describa la imagen')
+      p.mb-5(data-aos='fade-right') Como se puede observar en la imagen anterior, se visualiza la forma en cómo está distribuida la información con las diferentes entidades que hacen parte de esta.
+      .row.justify-content-center.mb-5
+        .col-lg-7.mb-lg-0.mb-3
+          p También se pueden visualizar los diferentes tipos de datos que tienen los atributos de la base de datos. Es necesario recordar que para relacionar dos entidades estas utilizan algo llamado llaves, las cuales pueden ser primarias o foráneas, y deben compartir la misma longitud y tipo de dato para poder relacionarse de manera correcta.
+          .tarjeta.color-secundario.p-4.mb-4
+            .row.justify-content-center.align-items-center
+              .col-lg-2.col-4(data-aos="fade-right"): img(src='@/assets/curso/temas/tema1/9.svg', alt='')
+              .col-lg-10
+                p.text-white.mb-0 En pocas palabras, si se van a relacionar 2 entidades y una tiene la llave de tipo int y la otra de tipo varchar, no se podrá realizar la conexión de las entidades, ya que estas tienen como restricción el tipo de dato y longitud para poder realizar el proceso de conexión de manera correcta. 
+        .col-lg-5.col-8: img(src='@/assets/curso/temas/tema7/18.png', alt='Texto que describa la imagen')
+      
 </template>
 
 <script>
@@ -26,4 +203,12 @@ export default {
 }
 </script>
 
-<style lang="sass"></style>
+<style lang="sass">
+.acordion__activo
+  background-color: #EBE7FF !important
+.f7
+  background-image: url('~@/assets/curso/temas/tema7/f7.svg')
+  background-repeat: no-repeat
+  background-position: left top
+  background-size: cover
+</style>
